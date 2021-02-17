e42a2ef8d757598a1814e8dea5c910b0554de496
tgrenaae <tgrena20@student.aau.dk>
2021-02-17 16:44:16 +0100

    put alfacode into functions

--- alfa.c
+++ alfa.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 //basic user struct - needs more parameters later
 typedef struct user {
     char name[25];
@@ -9,6 +8,10 @@ typedef struct user {
     char gender;
 } user;
 
+//prototypes
+int calc_users(FILE *userfile);
+void load_users(FILE *userfile, int totalusers, user *users);
+void printusers(int totalusers, user *users);
 
 
 int main(void){
@@ -23,16 +26,7 @@ int main(void){
     }
     
     //calculate number of users in database
-    char ch;
-    int totalusers = 0;
-    while(!feof(userfile)) {
-        ch = fgetc(userfile);
-        if(ch == '\n') {
-            totalusers++;    
-        }
-    }
-    totalusers++;
-    fseek(userfile, 0, SEEK_SET);
+    int totalusers = calc_users(userfile);
 
     //allocate memory for struct array with users
     user *users = (user *) malloc(totalusers * sizeof(user));
@@ -40,6 +34,30 @@ int main(void){
             printf("struct array error.");
             exit(EXIT_FAILURE);
         }
+
+    //load userinfo into array
+    load_users(userfile, totalusers, users);
+
+    //print userinfo
+    printusers(totalusers, users);
+
+    return 0;
+}
+
+int calc_users(FILE *userfile){
+    char ch;
+    int totalusers = 1;
+    while(!feof(userfile)) {
+        ch = fgetc(userfile);
+        if(ch == '\n') {
+            totalusers++;    
+        }
+    }
+    fseek(userfile, 0, SEEK_SET);
+    return totalusers;
+}
+
+void load_users(FILE *userfile, int totalusers, user *users){
 
     //scan in users from filepath
     for(int i = 0; i < (int) totalusers; i++){
@@ -50,11 +68,15 @@ int main(void){
         fgetc(userfile);
     }
 
+    return;
+}
+
+void printusers(int totalusers, user *users){
     //test that struct array is filled
     printf("total users: %d\n", totalusers);
     for(int i = 0; i < (int) totalusers; i++){
         printf("user %2d: name: %s, age: %d, gender %c\n", i+1, users[i].name, users[i].age, users[i].gender);
     }
-
-    return 0;
+    return;
 }
+
