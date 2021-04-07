e03a312c7681c8723013d22a62b06f07b1df3966
pelle112112 <73224725+pelle112112@users.noreply.github.com>
2021-04-07 11:08:39 +0200

    Clean chatrum

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+const name = prompt('Hvad er dit navn?')
+appendMessage('Du tilsluttede rummet')
+socket.emit('new-user', name)
+
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} tilsluttede rummet`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} forlod rummet`)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`Dig: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+  }
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const io = require('socket.io')(3000)
 
+
+const users = {}
+
 io.on('connection', socket => {
     console.log('new User')
-    socket.emit('chat-massage', 'Hello World')
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('bruger-tilsluttet', name)
+    })
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
+        socket.broadcast.emit('chat-message', {message: message, name:
+        users[socket.id]})
+    })
+    socket.on('disconnected', name => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users(socket.id)
     })
 })
