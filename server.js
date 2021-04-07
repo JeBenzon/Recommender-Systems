09273d6d2f8a3c9c65fb81304975f82a183b6105
pelle112112 <73224725+pelle112112@users.noreply.github.com>
2021-04-07 11:55:47 +0200

    Revert "Clean chatrum"
    This reverts commit e03a312c7681c8723013d22a62b06f07b1df3966.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const io = require('socket.io')(3000)
 
-
-const users = {}
-
 io.on('connection', socket => {
     console.log('new User')
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('bruger-tilsluttet', name)
-    })
+    socket.emit('chat-massage', 'Hello World')
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', {message: message, name:
-        users[socket.id]})
-    })
-    socket.on('disconnected', name => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users(socket.id)
+        socket.broadcast.emit('chat-message', message)
     })
 })
