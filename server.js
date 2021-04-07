290429dc64fe797d56aed92eb6580424b0807b52
pelle112112 <73224725+pelle112112@users.noreply.github.com>
2021-04-07 12:36:48 +0200

    Chat uden node modules

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
+const server = require('http').server(app)
+const io = require('socket.io')(server)
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = { name: {}}
+
+app.get('/', (req, res) => {
+  res.render('index', {rooms: rooms})
+})
+
+app.get('/:room', (res, req) => {
+  res.render('index', { roomsName: req.params.room })
+})
+
+server.listen(3000)
+
+const users = {}
 
 io.on('connection', socket => {
-    console.log('new User')
-    socket.emit('chat-massage', 'Hello World')
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
-    })
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
