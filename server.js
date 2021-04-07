af8c74131083d747014d0b7891c55a3525f43adc
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2021-04-07 13:49:33 +0200

    Toppen af bjerget

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { create } = require('domain')
 const express = require('express')
 const app = express()
 const server = require('http').server(app)
@@ -8,30 +9,55 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-const rooms = { name: {}}
+const rooms = {}
 
 app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms})
+  res.render('index', { rooms: rooms })
 })
 
-app.get('/:room', (res, req) => {
+
+app.post('/room', (res, req) => {
+  if (rooms[req.body.room != null]) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  //Send message that new room was created
+  io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
   res.render('index', { roomsName: req.params.room })
 })
 
 server.listen(3000)
 
-const users = {}
-
 io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', {
+      message: message,
+      name: rooms[room].users[socket.id]
+    })
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete users[socket.id]
+    })
   })
 })
+
+function getUserRooms(socket) {
+  return Object.entries(room).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
