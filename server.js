ea0fa7b7028f31087927807710e824b11befedf3
pelle112112 <73224725+pelle112112@users.noreply.github.com>
2021-04-14 11:18:39 +0200

    Bugfix for Linux
    ændrede broadcast.emit

--- server.js
+++ server.js
@@ -44,22 +44,35 @@ server.listen(3000) //Få server til at lytte til den rigtige port
 
 io.on('connection', socket => { //Første gang bruger loader hjemmeside -> kalder funktion og giver dem et socket
   socket.on('new-user', (room, name) => { //Funktion bliver kaldt i "scripts.js"
-    socket.join(room)
-    rooms[room].users[socket.id] = name //Sammensætter navn på bruger med socket id
-    socket.to(room).broadcast.emit('user-connected', name) //Sender event 'user-connected' med besked "name" -> broadcast gør så brugeren ikke selv får det
+    try{
+      socket.join(room)
+      rooms[room].users[socket.id] = name //Sammensætter navn på bruger med socket id
+      socket.broadcast.to(room).emit('user-connected', name) //Sender event 'user-connected' med besked "name" -> broadcast gør så brugeren ikke selv får det
+    }catch(e){
+      console.log(e)
+    }
   })
   socket.on('send-chat-message', (room, message) => { //Aktivere når eventen sker "Send-chat-message" med data "room" "message"
-    socket.to(room).broadcast.emit('chat-message', { //Sender beskeden til alle undtagen brugeren som sender den selv "broadcast" gør så brugeren ikke selv modtager
+    try{
+      socket.broadcast.to(room).emit('chat-message', { //Sender beskeden til alle undtagen brugeren som sender den selv "broadcast" gør så brugeren ikke selv modtager
 
       //TODO her skal vi gemme ned i vores array/filer
       message: message, //Laver et objekt til at holde dataen på beskeden
       name: rooms[room].users[socket.id] //Tilføjer navnet til objeket via socket.id
     })
+    }catch(e){
+      console.log(e)
+    }
+    
   })
   socket.on('disconnect', () => { //Aktivere når en disconnecter -> socket funktion
     getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id]) //Sender ud at brugeren er disconnected
-      delete rooms[room].users[socket.id] //Sletter brugeren && bruger id 
+      try{
+        socket.broadcast.to(room).emit('user-disconnected', rooms[room].users[socket.id]) //Sender ud at brugeren er disconnected
+        delete rooms[room].users[socket.id] //Sletter brugeren && bruger id 
+      }catch(e){
+        console.log(e)
+      }
     })
   })
 })
