e072d2609318beb4fa7198203b6eaa9bc4d20cb5
hackerkarlos <karlemilhertz@gmail.com>
2021-05-05 10:15:39 +0200

    Updated chatMessage

--- src/functions.js
+++ src/functions.js
@@ -371,31 +371,31 @@ function chatMessage(index){
     let paraMessage = ""
     switch (index){
         case 0:
-            paraMessage = "Hvad er dit yndlings fodboldhold?";
+            paraMessage = "I har begge vist interrese for sport - Hvad er jeres yndlings sport?";
             break;
         case 1:
-            paraMessage = "Hvad din yndlings ret?";
+            paraMessage = "I har begge vist interrese for mad - Hvad er jeres yndlings ret?";
             break;
         case 2:
-            paraMessage = "Hvem er din yndlings kunster?";
+            paraMessage = "I har begge vist interrese for kunst - Hvad er jeres yndlings kunstner";
             break;
         case 3:
-            paraMessage = "Hvad er din yndlings film?";
+            paraMessage = "I har begge vist interrese for film - Hvad er jeres yndlings film?";
             break;
         case 4:
-            paraMessage = "Hvem er din yndlings kunster?";
+            paraMessage = "I har begge vist interrese for musik - Hvad er jeres yndlings genre?";
             break;
         case 5:
-            paraMessage = "Hvad er din yndlings udendørs aktivitet?";
+            paraMessage = "I har begge vist interrese for udendør afktiviteter - Hvad er jeres yndlings udendørs aktivitet?";
             break;
         case 6:
-            paraMessage = "Hvad er din yndlings grundstof?";
+            paraMessage = "I har begge vist interrese for videnskab - Hvad er jeres yndlings grundstof?";
             break;
         case 7:
-            paraMessage = "Hvad er dit drømme rejsemål";
+            paraMessage = "I har begge vist interrese for rejse - Hvad er jeres drømme rejsemål?";
             break;
         case 8:
-            paraMessage = "Hvorfor er klimaet vigtigt for dig?";
+            paraMessage = "I har begge vist interrese for klimaet - Hvorfor er klimaet vigtigt for dig?";
             break;
     }
     return paraMessage
